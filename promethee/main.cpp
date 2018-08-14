9dea7b9d767ddfcafe145923765ed53e8cbb7039
Filipe Mendonça <filipetrm@gmail.com>
2018-08-14 14:45:47 -0300

    promethee now receives input path as argument

--- promethee/main.cpp
+++ promethee/main.cpp
@@ -6,6 +6,7 @@
 #include "inputreader.h"
 #include "outputwriter.h"
 #include <iostream>
+#include <dirent.h>
 using namespace std;
 const string pathToInputDirectory = "promethee/input/";
 const string pathToOutputDirectory = "promethee/output/";
@@ -14,13 +15,53 @@ int main(int argc, char *argv[]){
   Data data = Data();
   InputReader inputReader = InputReader();
 
+  vector<string> valFiles, metaFiles;
+
+  DIR           *dirp;
+  struct dirent *directory;
+
+  for (int i = 1; i < argc; i++) {
+    
+    dirp = opendir(argv[i]);
+    if (dirp) {   
+      while ((directory = readdir(dirp)) != NULL) {
+
+        string fileName(directory->d_name);
+        if(fileName != "." && fileName != ".." && fileName != ".DS_Store") {
+          if (i == 1) {                     // path to values must be the first one
+            valFiles.push_back(fileName);
+          } else if (i == 2) {              // path to metadata must be the second one
+            metaFiles.push_back(fileName);
+          } 
+        }
+      }
+      closedir(dirp);
+    } else {
+      cout << "Directory " << argv[i] << "not found " << endl;
+      return 0;
+    }
+  }
+
+  sort(valFiles.begin(), valFiles.end());
+  sort(metaFiles.begin(), metaFiles.end());
+
+  if(valFiles.size() != metaFiles.size()) { // files dont match
+    cout << "error\n";
+    return 0;
+  }
+
   // return 0;
-  for(int i = 1; i < argc; i += 1){
-    string fileName(argv[i]);
+  for(int i = 0; i < valFiles.size(); i++){
+    string valuePath(argv[1]);
+    string metaPath(argv[2]);
+
+
+    cout << valuePath + valFiles[i] << endl;
+    cout << metaPath + metaFiles[i] << endl;
 
     // reading matrix of values in which each cell represent the pixel's value for a certain criteria
-    Matrix nmatrix = inputReader.readMatrix(pathToInputDirectory + fileName);
-    MatrixMetaData metaData = inputReader.readMetaData(pathToInputDirectory + fileName + "_p");
+    Matrix nmatrix = inputReader.readMatrix(valuePath + valFiles[i]);
+    MatrixMetaData metaData = inputReader.readMetaData(metaPath + metaFiles[i]);
 
     // reading the criteria weight
     // ldouble weight = atof(argv[i + 1]);
