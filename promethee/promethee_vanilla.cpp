91d9f45ce6d8ee9ac16767d8e3ad21e54aa4b690
Gabriel Gomes <gabriel.gomes@ccc.ufcg.edu.com>
2018-08-31 16:43:38 -0300

    otimizating vanilla

--- promethee/promethee_vanilla.cpp
+++ promethee/promethee_vanilla.cpp
@@ -17,23 +17,24 @@ PrometheeResult PrometheeVanilla::process(Data data){
 		Matrix matrix = data.getCriteriaMatrix(criteria);
 		ldouble weight = data.getCriteriaWeight(criteria);
 		PrometheeFunctionAdapter* function = data.getFunction(criteria);
+		ComparisonFunction comparison = (*(*function).getVanilla());
 		bool isMax = data.getIsMax(criteria);
 		vector<ldouble> values;
 		for(int line = 0; line < nlines; line++){
 			cerr << (100.0 * (criteria * nlines + line + 1))/(ncriterias * nlines) << "%"<< endl;
 			for(int column = 0; column < ncolumns; column++){
 				if(!validPixels[line][column]) continue;
-				for(int line2 = 0; line2 < nlines; line2++){
-					for(int column2 = 0; column2 < ncolumns; column2++){
+				for(register int line2 = 0; line2 < nlines; line2++){
+					for(register int column2 = 0; column2 < ncolumns; column2++){
 						if(validPixels[line2][column2] && 
 						(line != line2 || column != column2)){
 
 							if(isMax){
-								positiveFlow[line][column] += weight * (*(*function).getVanilla())(matrix[line][column], matrix[line2][column2]);
-								negativeFlow[line][column] += weight * (*(*function).getVanilla())(matrix[line2][column2], matrix[line][column]);
+								positiveFlow[line][column] += weight * comparison(matrix[line][column], matrix[line2][column2]);
+								negativeFlow[line][column] += weight * comparison(matrix[line2][column2], matrix[line][column]);
 							} else {
-								positiveFlow[line][column] += weight * (*(*function).getVanilla())(matrix[line2][column2], matrix[line][column]);
-								negativeFlow[line][column] += weight * (*(*function).getVanilla())(matrix[line][column], matrix[line2][column2]);
+								positiveFlow[line][column] += weight * comparison(matrix[line2][column2], matrix[line][column]);
+								negativeFlow[line][column] += weight * comparison(matrix[line][column], matrix[line2][column2]);
 							}
 						}
 					}
