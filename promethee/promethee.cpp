becaf39ce40fa954b4d0a9736aaeb7849e31bfb1
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2018-09-20 09:30:29 -0300

    Creating abstract class of promethee

---
+++ promethee/promethee.cpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "promethee.h"
+
+Promethee::Promethee(vector<string> inputPaths, vector<string> metaPaths){
+
+}
+
+void Promethee::process(){
+
+}
---
+++ promethee/promethee.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+#include <string>
+using namespace std;
+
+struct Promethee{
+    Promethee(vector<string> inputPaths, vector<string> metaPaths){
+        
+    }
+
+    void process();
+};
