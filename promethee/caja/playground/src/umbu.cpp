88af4adcd52eddfcb23494e95d09d16a0e90ec1f
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2018-09-21 10:54:23 -0300

    Less binary searches in umbu caja

--- promethee/caja/playground/src/umbu.cpp
+++ promethee/caja/playground/src/umbu.cpp
@@ -27,20 +27,16 @@ struct LinearFunction{
     ldouble getPositiveDelta(vector<ldouble> &values, ldouble queryValue, vector<ldouble> &cummulative, ldouble weight){
         ldouble sum = 0;
         {
-            int ptr = lower_bound(values.begin(), values.end(), queryValue - pParameter) - values.begin();
-            ldouble value = weight * ptr;
-            sum += value;
-        }
-        {
-            int ptr = lower_bound(values.begin(), values.end(), queryValue) - values.begin();
             int ptr2 = lower_bound(values.begin(), values.end(), queryValue - pParameter) - values.begin();
+            ldouble value = weight * ptr2;
+            sum += value;
+
+            int ptr = lower_bound(values.begin(), values.end(), queryValue) - values.begin();
             int amount = ptr - ptr2;
-            if (amount > 0){
+            if(amount > 0){
                 ldouble value = 0;
-                if (ptr > 0)
-                    value += cummulative[ptr - 1];
-                if (ptr2 > 0)
-                    value -= cummulative[ptr2 - 1];
+                if(ptr > 0) value += cummulative[ptr - 1];
+                if(ptr2 > 0) value -= cummulative[ptr2 - 1];
                 ldouble fvalue = weight * (amount * queryValue - value) / pParameter;
                 sum += fvalue;
             }
@@ -50,24 +46,20 @@ struct LinearFunction{
     ldouble getNegativeDelta(vector<ldouble> &values, ldouble queryValue, vector<ldouble> &cummulative, ldouble weight){
         ldouble sum = 0;
         {
-            int ptr = upper_bound(values.begin(), values.end(), queryValue + pParameter) - values.begin();
-            if (ptr < values.size()){
-                ldouble value = weight * (values.size() - ptr);
+            int ptr2 = upper_bound(values.begin(), values.end(), queryValue + pParameter) - values.begin();
+            if(ptr2 < values.size()){
+                ldouble value = weight * (values.size() - ptr2);
                 sum += value;
             }
-        }
-        {
+            
             int ptr = upper_bound(values.begin(), values.end(), queryValue) - values.begin();
-            int ptr2 = upper_bound(values.begin(), values.end(), queryValue + pParameter) - values.begin();
             int amount = ptr2 - ptr;
-            if (amount > 0){
+            if(amount > 0){
                 ldouble value = 0;
-                if (ptr2 > 0)
-                    value += cummulative[ptr2 - 1];
-                if (ptr > 0)
-                    value -= cummulative[ptr - 1];
+                if(ptr2 > 0) value += cummulative[ptr2 - 1];
+                if(ptr > 0) value -= cummulative[ptr - 1];
                 ldouble fvalue = weight * (value - amount * queryValue) / pParameter;
-                sum += fvalue;
+                sum += fvalue;        
             }
         }
         return sum;
@@ -75,13 +67,11 @@ struct LinearFunction{
     ldouble getPositiveDelta(vector<ldouble> &values, ldouble queryValue, vector<ldouble> &cummulative, ldouble weight, vector<unsigned int> &cnt){
         ldouble sum = 0;
         {
-            int ptr = lower_bound(values.begin(), values.end(), queryValue - pParameter) - values.begin();
-            ldouble value = weight * ptr;
-            sum += value;
-        }
-        {
-            int ptr = lower_bound(values.begin(), values.end(), queryValue) - values.begin();
             int ptr2 = lower_bound(values.begin(), values.end(), queryValue - pParameter) - values.begin();
+            ldouble value = weight * (ptr2 > 0 ? cnt[ptr2 - 1] : 0);
+            sum += value;
+        
+            int ptr = lower_bound(values.begin(), values.end(), queryValue) - values.begin();
             int amount = 0;
             if(ptr > 0) amount += cnt[ptr - 1];
             if(ptr2 > 0) amount -= cnt[ptr2 - 1];
@@ -100,15 +90,13 @@ struct LinearFunction{
     ldouble getNegativeDelta(vector<ldouble> &values, ldouble queryValue, vector<ldouble> &cummulative, ldouble weight, vector<unsigned int> &cnt){
         ldouble sum = 0;
         {
-            int ptr = upper_bound(values.begin(), values.end(), queryValue + pParameter) - values.begin();
-            if (ptr < values.size()){
-                ldouble value = weight * (values.size() - ptr);
+            int ptr2 = upper_bound(values.begin(), values.end(), queryValue + pParameter) - values.begin();
+            if (ptr2 < values.size()){
+                ldouble value = weight * (cnt.back() - (ptr2 > 0 ? cnt[ptr2 - 1] : 0));
                 sum += value;
             }
-        }
-        {
+
             int ptr = upper_bound(values.begin(), values.end(), queryValue) - values.begin();
-            int ptr2 = upper_bound(values.begin(), values.end(), queryValue + pParameter) - values.begin();
             int amount = 0;
             if(ptr > 0) amount += cnt[ptr - 1];
             if(ptr2 > 0) amount -= cnt[ptr2 - 1];
