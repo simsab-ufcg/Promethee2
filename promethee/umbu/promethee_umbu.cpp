5ed4d25d5f1c5324c8b89bac9bf6bdfb8c2cc927
Thiago Manel <thiago.manel@gmail.com>
2018-10-05 17:34:30 -0300

    remove unecessary logging line

--- promethee/umbu/promethee_umbu.cpp
+++ promethee/umbu/promethee_umbu.cpp
@@ -21,18 +21,14 @@
 void PrometheeUmbu::init(vector<string> args, int divideBy){
     this->divideBy = divideBy;
 
-    /**
-     * Get type of function
-     * */
+    // Get type of function
     string type = getCmdOption(args, "-type");
     if(!type.size() || (type != "linear" && type != "linearWithIndifference")){
         cerr << "Error: incorrect arguments." << endl;
         exit(0);
     }
-    
-    /**
-     * Get size of chunk
-     * */
+
+    // Get size of chunk
     string chunk = getCmdOption(args, "-chunk");
     if(!chunk.size()){
         cerr << "Error: incorrect arguments." << endl;
@@ -42,26 +38,18 @@ void PrometheeUmbu::init(vector<string> args, int divideBy){
 
     this->isMax = hasFlag(args, "-ismax");
 
-    /**
-     * args[0] should be the filename by now, since there is no more flags
-     * */
+    // args[0] should be the filename by now, since there is no more flags
     this->filename = args[0];
-    
-    /**
-     * args[1] is the weight
-     * */
+
+    // args[1] is the weight
     this->weight = atof(args[1].c_str());
 
-    /**
-     * the rest of args is the arguments of the function
-     * */
+    // the remaining args are function args
     vector<ldouble> params;
     for(int i = 2; i < args.size(); i++)
         params.push_back(atof(args[i].c_str()));
 
-    /**
-     * Setup function
-     * */
+    // build specific funcion implementation
     if(type == "linear"){
         this->function = new LinearUmbuFunction(params);
     } else if(type == "linearWithIndifference"){
@@ -73,11 +61,11 @@ void PrometheeUmbu::init(vector<string> args, int divideBy){
  * Add contribution of a chunk to all alternatives
  * outputFile is where currently is the output
  * nextFile is to where the output should be sent
- * 
+ *
  * input, values, sumAccum, cnt are arguments to make calc of flow
  * */
 void PrometheeUmbu::generateChunkOutTifUnbu(string &outputFile, string &nextFile, TIFF *input, vector<ldouble> & values, vector<ldouble> & sumAccum, vector<unsigned int> &cntAccum){
-    
+
     TIFF *out = TIFFOpen(outputFile.c_str(), "rm");
     TIFF *nxt = openFile(nextFile, this->width, this->height);
 
@@ -89,30 +77,23 @@ void PrometheeUmbu::generateChunkOutTifUnbu(string &outputFile, string &nextFile
         TIFFReadScanline(out, outline, i);
         for (int j = 0; j < this->width; j++) {
             if(isnan(line[j])) {
-                /**
-                 * Write nan to correspond with input
-                 * */
+                // Write nan to correspond with input
                 outline[j] += -sqrt(-1.0); // ?? this should be nan
             } else{
                 ldouble positiveDelta = (*this->function).getPositiveDelta(values, line[j], sumAccum, weight, cntAccum);
                 ldouble negativeDelta = (*this->function).getNegativeDelta(values, line[j], sumAccum, weight, cntAccum);
-                /**
-                 * if is not max function, just need to invert negative with positive
-                 * */
+                // if is not max function, just need to invert negative with positive
                 int mul = this->isMax ? 1 : -1;
                 outline[j] += mul * (positiveDelta - negativeDelta);
             }
         }
-	
+
         TIFFWriteScanline(nxt, outline, i);
     }
     TIFFClose(nxt);
     TIFFClose(out);
 
-    /**
-     * Output is swapped with the next file, 
-     * to keep logic of filenaming
-     * */
+    // Output is swapped with the next file to keep logic of filenaming
     swap(outputFile, nextFile);
 }
 
@@ -126,11 +107,9 @@ void PrometheeUmbu::divide(string &outputFile, string &nextFile, TIFF *input){
     ldouble *line = new ldouble[this->width];
     ldouble *outline = new ldouble[this->width];
 
-    /**
-     * 
-     * */
+    //TODO:
     int denominator = (this->divideBy != -1 ? this->divideBy : this->area - 1);
-    
+
     for (int i = 0; i < this->height; i++){
 
         TIFFReadScanline(input, line, i);
@@ -142,7 +121,7 @@ void PrometheeUmbu::divide(string &outputFile, string &nextFile, TIFF *input){
                 outline[j] /= denominator;
             }
         }
-	
+
         TIFFWriteScanline(nxt, outline, i);
     }
     TIFFClose(nxt);
@@ -154,24 +133,18 @@ void PrometheeUmbu::divide(string &outputFile, string &nextFile, TIFF *input){
  * Preprocess required data based in map
  * */
 void PrometheeUmbu::processChunk(map<double, int> & cnt, string & outputFile, string & nextFile, TIFF * input){
-    /**
-     * Get the keys of the map
-     * */
+    // Get the keys of the map
     vector<ldouble> values;
     for(auto it : cnt)
         values.push_back(it.first);
-    /**
-     * Makes accumulated sum considering frequency of every element and its value
-     * */
+    // Makes accumulated sum considering frequency of every element and its value
     vector<ldouble> sumAcc;
     ldouble sum = 0;
     for(auto &it : cnt) {
         sum += it.first * it.second;
         sumAcc.push_back(sum);
     }
-    /**
-     * Makes accumulated sum of the frequency of the elements
-     * */
+    // Makes accumulated sum of the frequency of the elements
     vector<unsigned int> countAcc;
     unsigned int scnt = 0;
     for(auto &it: cnt){
@@ -185,37 +158,27 @@ void PrometheeUmbu::processChunk(map<double, int> & cnt, string & outputFile, st
 
 void PrometheeUmbu::process(){
 
-    /**
-     * Open input and get fields need to create similar file for output
-     * */
+    // Open input and get fields need to create similar file for output
     TIFF *input = TIFFOpen(this->filename.c_str(), "rm");
     TIFFGetField(input, TIFFTAG_IMAGEWIDTH, &this->width);
     TIFFGetField(input, TIFFTAG_IMAGELENGTH, &this->height);
     TIFFGetField(input, TIFFTAG_SAMPLESPERPIXEL, &this->samplePerPixel);
 
-    /**
-     * Name of files to use during processing
-     * */
+    // Name of files to use during processing
     string nextFile = "nxt." + this->filename;
     string outputFile = "out." + this->filename;
 
-    /**
-     * Create output file
-     * */
+    // Create output file
     setupOutput(outputFile, this->width, this->height);
 
     ldouble *line = new ldouble[width];
 
-    /**
-     * To contabilize distinct values
-     * */
+    // To contabilize distinct values
     map<double, int> cnt;
-    
+
     this->area = 0;
     for(int i = 0; i < height; i++){
-        /**
-         * Read line of input and add to map and area
-         * */
+        // Read line of input and add to map and area
         TIFFReadScanline(input, line, i);
         for(int j = 0; j < width; j++){
             if(!isnan(line[j])){
@@ -224,37 +187,25 @@ void PrometheeUmbu::process(){
             }
         }
         if(cnt.size() >= this->chunkBound){
-            /**
-             * A chunk is full, time to add conribution
-             * */
+            // A chunk is full, time to add conribution
             this->processChunk(cnt, outputFile, nextFile, input);
         }
     }
 
 
     if(cnt.size() > 0){
-        /**
-         * An incomplete chunk, add his contribution
-         * */
+        // An incomplete chunk, add his contribution
         this->processChunk(cnt, outputFile, nextFile, input);
     }
 
-    /**
-     * Divide
-     * */
+    // Divide
     this->divide(outputFile, nextFile, input);
 
-    /**
-     * Maybe after all processing the result be in nxt.filename,
-     * to fix this we rename the file in case it happens,
-     * with this always the result is in out.filename
-     * */
+    // Maybe after all processing the result be in nxt.filename, to fix this
+    // we rename the file in case it happens, with this always the result is in out.filename
     if(outputFile[0] != 'o'){
         rename(outputFile.c_str(), nextFile.c_str());
     }
 
-    /**
-     * 
-     * */
     TIFFClose(input);
 }
