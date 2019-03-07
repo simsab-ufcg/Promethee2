632c063239e5b870f3ed50c95aab4409bd8ee7ed
Gabriel Gomes <gabriel.gomes@ccc.ufcg.edu.br>
2019-03-07 10:33:38 -0300

    Promethee optimized using tiff reader

--- promethee/optimized/promethee_opt.cpp
+++ promethee/optimized/promethee_opt.cpp
@@ -2,112 +2,117 @@
 #include "../normalize.h"
 #include "../promethee_function_adapter.h"
 #include <iostream>
+#include "../functions/optimized/linear_function.h"
+#include "../functions/optimized/linear_with_indifference_function.h"
 #include "../inputreader.h"
 #include "../outputwriter.h"
+#include "../plibtiff.h"
 #include "../parse_directory.h"
-
-Data PrometheeOpt::readData(){
-  InputReader inputReader = InputReader();
-  Data data = Data();
-  for(int i = 0; i < this->inputFiles.size(); i++){
-    Matrix nmatrix = inputReader.readMatrix(this->inputFiles[i]);
-    MatrixMetaData metaData = inputReader.readMetaData(this->metaFiles[i], false);
-    data.addCriteria(nmatrix, metaData);
-  }
-  data.normalizeWeights();
-  return data;
-}
+#include "../parse_args.h"
 
 void PrometheeOpt::init(vector<string> args, int divideBy){
   this->divideBy = divideBy;
-  parseInputAndMeta(args, this->inputFiles, this->metaFiles, this->pathToOutput);
+  
+  // Get type of function
+  string type = getCmdOption(args, "-type");
+  if(!type.size() || (type != "linear" && type != "linearWithIndifference")){
+      cerr << "Error: incorrect arguments." << endl;
+      exit(0);
+  }
+
+  this->isMax = hasFlag(args, "-ismax");
+
+  // args[0] should be the filename by now, since there is no more flags
+  this->filename = args[0];
+
+  // args[1] is the weight
+  this->weight = atof(args[1].c_str());
+
+  // the remaining args are function args
+  vector<ldouble> params;
+  for(int i = 2; i < args.size(); i++)
+      params.push_back(atof(args[i].c_str()));
+
+  // build specific funcion implementation
+  if(type == "linear"){
+      this->function = new LinearFunction(params);
+  } else if(type == "linearWithIndifference"){
+      this->function = new LinearWithIndifferenceFunction(params);
+  }
 }
+
 
 void PrometheeOpt::process() {
 
-  Data data = this->readData();
-  int ncriterias = data.matrices.size();
-  int nlines = data.matrices[0].size();
-  int ncolumns = data.matrices[0][0].size();
+  // Open input and get fields need to create similar file for output
+  TIFF *input = TIFFOpen(this->filename.c_str(), "rm");
+  TIFFGetField(input, TIFFTAG_IMAGEWIDTH, &this->width);
+  TIFFGetField(input, TIFFTAG_IMAGELENGTH, &this->height);
+  TIFFGetField(input, TIFFTAG_SAMPLEFORMAT, &this->sampleFormat);
+  TIFFGetField(input, TIFFTAG_SAMPLESPERPIXEL, &this->samplePerPixel);
 
-  // getting matrix with information about valid pixels
-  MaskMatrix validPixels = data.getMaskMatrix();
+  Matrix netFlow = Matrix(this->height, MatrixLine(this->width, 0.0));
 
-  Matrix positiveFlow = Matrix(nlines, MatrixLine(ncolumns, 0.0));
-  Matrix negativeFlow = Matrix(nlines, MatrixLine(ncolumns, 0.0));
-  Matrix netFlow = Matrix(nlines, MatrixLine(ncolumns, 0.0));
-  
-  int studyArea = 0;
-  // applying the optimized promethee method (using linear preference function only)
-  for(int criteria = 0; criteria < ncriterias; criteria++){
+  // Create PixelReader Interface
+  unsigned short byte_size = TIFFScanlineSize(input) / this->width;
+  tdata_t line = _TIFFmalloc(TIFFScanlineSize(input));
+  PixelReader pr = PixelReader(this->sampleFormat, byte_size, line);
 
-    Matrix matrix = data.getCriteriaMatrix(criteria);
-    ldouble weight = data.getCriteriaWeight(criteria);
-    PrometheeFunctionAdapter* function = data.getFunction(criteria);
-    bool isMax = data.getIsMax(criteria);
-
-    vector<ldouble> values;
-    for(int line = 0; line < nlines; line++) {
-      for(int column = 0; column < ncolumns; column++) {
-        if(validPixels[line][column]) {
-          values.push_back(matrix[line][column]);
-        }
-      }
-    }
-
-    sort(values.begin(), values.end());
-
-    studyArea = values.size();
-    int nvalues = values.size();
-    vector<ldouble> cummulative(nvalues, 0);
-
-    cummulative[0] = values[0];
-    for(int i = 1; i < nvalues; i++) {
-      cummulative[i] = cummulative[i - 1] + values[i];
-    }
-
-    for(int line = 0; line < nlines; line++){
-      for(int column = 0; column < ncolumns; column++){
-        if(validPixels[line][column]){
-
-          if(isMax){
-            positiveFlow[line][column] += (*function).getPositiveDelta(values, matrix[line][column], cummulative, weight);
-            negativeFlow[line][column] += (*function).getNegativeDelta(values, matrix[line][column], cummulative, weight);
-          } else {
-            negativeFlow[line][column] += (*function).getPositiveDelta(values, matrix[line][column], cummulative, weight);
-            positiveFlow[line][column] += (*function).getNegativeDelta(values, matrix[line][column], cummulative, weight);
-          }
-
-        }
+  vector<ldouble> values;
+  for(int i = 0; i < this->height; i++) {
+    TIFFReadScanline(input, line, i);
+    for(int j = 0; j < this->width; j++) {
+      double aux = pr.readPixel(j);
+      if(!isnan(aux)) {
+        values.push_back(aux);
       }
     }
   }
 
-  int denominator = (this->divideBy != -1 ? this->divideBy : studyArea - 1);
+  sort(values.begin(), values.end());
+
+  int nvalues = values.size();
+  vector<ldouble> cummulative(nvalues, 0);
+
+  cummulative[0] = values[0];
+  for(int i = 1; i < nvalues; i++) {
+    cummulative[i] = cummulative[i - 1] + values[i];
+  }
+
+  for(int i = 0; i < this->height; i++){
+    TIFFReadScanline(input, line, i);
+    for(int j = 0; j < this->width; j++){
+      if(!isnan(pr.readPixel(j))){
+
+        if(this->isMax){
+          netFlow[i][j] += (*this->function).getPositiveDelta(values, pr.readPixel(j), cummulative, this->weight);
+          netFlow[i][j] -= (*this->function).getNegativeDelta(values, pr.readPixel(j), cummulative, this->weight);
+        } else {
+          netFlow[i][j] -= (*this->function).getPositiveDelta(values, pr.readPixel(j), cummulative, this->weight);
+          netFlow[i][j] += (*this->function).getNegativeDelta(values, pr.readPixel(j), cummulative, this->weight);
+        }
+
+      }
+    }
+  }
+
+  int denominator = (this->divideBy != -1 ? this->divideBy : nvalues - 1);
   // applying a not standard normalization (but used by grass)
-  for(int line = 0; line < nlines; line++)
-    for(int column = 0; column < ncolumns; column++){
-      positiveFlow[line][column] /= denominator;
-      negativeFlow[line][column] /= denominator;
+  for(int i = 0; i < this->height; i++)
+    for(int j = 0; j < this->width; j++){
+      netFlow[i][j] /= denominator;
     }
 
-  // calculating global flow
-  for(int line = 0; line < nlines; line++) {
-    for(int column = 0; column < ncolumns; column++) {
-      netFlow[line][column] = positiveFlow[line][column] - negativeFlow[line][column];
+  TIFF *output = openFile("out." + this->filename, this->width, this->height);
+
+  double write_line[this->width];
+  for(int i = 0; i < this->height; i++){
+    for(int j = 0; j < this->width; j++){
+      write_line[j] = netFlow[i][j];
     }
+    TIFFWriteScanline(output, write_line, i);
   }
 
-  // generating results 
-  PrometheeResult result = PrometheeResult();
-  result.positiveFlow = positiveFlow;
-  result.negativeFlow = negativeFlow;
-  result.netFlow = netFlow;
-  result.validPixels = validPixels;
-
-  // normalizing results
-  result.normalizedFlow = Normalizer().normalize(netFlow, validPixels);
-  
-  OutputWriter outputWriter = OutputWriter();
-  outputWriter.write(this->pathToOutput, result);
+  TIFFClose(output);
+  TIFFClose(input);
 }
--- promethee/optimized/promethee_opt.h
+++ promethee/optimized/promethee_opt.h
@@ -3,16 +3,26 @@
   #define PROMETHEE_INCLUDED
   
   #include "../types.h"
+  #include "../utils.h"
   #include "../data.h"
   #include "../promethee.h"
+  #include "../functions/optimized/promethee_function.h"
   #include <algorithm>
   
   struct PrometheeOpt : Promethee {
 
-    string pathToOutput;
-    vector<string> inputFiles, metaFiles;
+    string filename; // name of criterion to be processed
+    double weight; // weight of criterion
+    uint16 sampleFormat; // size of bytes
+    PrometheeFunction *function; // function to be used in processing
+    int divideBy; // division constant , -1 to divide by (Area - 1)
+    int chunkBound; // max chunk size
+    bool isMax; // kind of function
+    int area; // area of criterion
 
-    Data readData();
+    // info related to criterion file
+    int height, width, samplePerPixel;
+
     void process();
     void init(vector<string> args, int divideBy);
   };
